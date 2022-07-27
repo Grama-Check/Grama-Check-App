af9cb6e8b43779ba38bf3a8e8567402db9402b3f
jhivanb <jhivanb@gmail.com>
2022-07-27 22:13:57 +0530

    index endpoint working

---
+++ handlers.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Index(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"title": "testing",
+		},
+	)
+}
--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	router.LoadHTMLGlob("/templates/*")
+	router.LoadHTMLGlob("templates/*")
 	router.GET("/", Index)
-	router.GET("/signup", Signup)
-	router.GET("/login", Login)
+	//router.GET("/signup", Signup)
+	//router.GET("/login", Login)
 
 	//authGroup := router.Group("/").Use()
-	router.GET("/home", Home)
+	//router.GET("/home", Home)
 	router.POST("/gramacheck")
 	// -> Identity Check , id, address , UID
 	// <- Failed/Passed , UID , Pass/Fail
-
+	router.Run()
 }
