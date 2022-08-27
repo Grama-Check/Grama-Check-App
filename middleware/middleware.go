2803d55ee63f978db92de6c4ab75925f618f2d93
HarshaSenarath <harsha.senarath13@gmail.com>
2022-08-27 12:05:12 +0530

    updated commits in middleware.go

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -12,47 +12,49 @@ import (
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Checking if a token is sent in the Authorization Header
+		// checking if a token is present in the Authorization Header
 		token := c.GetHeader("Authorization")
-		
+
 		if len(token) == 0 {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, "No token present")
 			return
 		}
-		
-		
+
 		fields := strings.Fields(token)
-		
-		// Checking if a token has prefix bearer
+
+		// checking if a token has prefix bearer
 		if len(fields) < 2 {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, "Single string no prefix")
 			return
 		}
-		
-		// Checking if the prefix is equal to "brearer"
+
+		// checking if the prefix is equal to "brearer"
 		if strings.ToLower(fields[0]) != "bearer" {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, "Incorrect prefix")
 			return
 		}
 
 		accessToken := fields[1]
-		
-		// Getting user details from asgardeo
+
+		// getting user details from asgardeo
+		// configuring the request
 		req, err1 := http.NewRequest("GET", "https://api.asgardeo.io/t/gramacheck/oauth2/userinfo", nil)
 		if err1 != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, err1.Error())
 			return
 		}
 
+		// setting the authorization header in the request
 		req.Header.Set("Authorization", "Bearer "+accessToken)
 
+		// making the request to asgardeo
 		resp, err2 := http.DefaultClient.Do(req)
 		if err2 != nil {
 			c.AbortWithStatusJSON(http.StatusInternalServerError, err2.Error())
 			return
 		}
-		
-		// If the expected response is not recieved an issue with access token
+
+		// struct to store information receiving from Asgardeo
 		user := models.AuthorizedUser{}
 
 		err4 := json.NewDecoder(resp.Body).Decode(&user)
@@ -60,17 +62,18 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.AbortWithStatusJSON(http.StatusBadRequest, err4.Error())
 			return
 		}
-		
-		// If the expected response is 
+
+		// if the expected response is not recieved an issue with access token
 		if (models.AuthorizedUser{} == user) {
 			c.AbortWithStatusJSON(http.StatusUnauthorized, "Access token validation failed")
 			return
 		}
 
 		formType := c.GetHeader("Form")
-		
-		// Checking if the request form was sent by submitting the apply form or check form
+
+		// checking if the request form was sent by submitting the apply form or check form
 		if formType == "apply" {
+			// struct to store infotmation recived upon apply form submission
 			model := models.Person{}
 
 			if err := c.ShouldBindBodyWith(&model, binding.JSON); err != nil {
@@ -78,14 +81,17 @@ func AuthMiddleware() gin.HandlerFunc {
 				return
 			}
 
+			// removing leading trailing whitespaces from the NICs
 			formNIC := strings.ReplaceAll(model.NIC, " ", "")
 			userNIC := strings.ReplaceAll(user.NIC, " ", "")
 
+			// comparing the NICs received from Asgardeo and the form
 			if !strings.EqualFold(formNIC, userNIC) {
 				c.AbortWithStatusJSON(http.StatusUnauthorized, "NIC mismatch")
 				return
 			}
 		} else if formType == "check" {
+			// struct to store infotmation recived upon check form submission
 			model := models.StatusCheck{}
 
 			if err := c.ShouldBindBodyWith(&model, binding.JSON); err != nil {
@@ -93,9 +99,11 @@ func AuthMiddleware() gin.HandlerFunc {
 				return
 			}
 
+			// removing leading trailing whitespaces from the NICs
 			formNIC := strings.ReplaceAll(model.NIC, " ", "")
 			userNIC := strings.ReplaceAll(user.NIC, " ", "")
 
+			// comparing the NICs received from Asgardeo and the form
 			if !strings.EqualFold(formNIC, userNIC) {
 				c.AbortWithStatusJSON(http.StatusUnauthorized, "NIC mismatch")
 				return
