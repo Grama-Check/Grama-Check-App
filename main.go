8d77efa9abd50137e404778a27a963cf89c6f483
jhivandebenoit <jhivanb@gmail.com>
2022-08-29 12:41:42 +0530

    updated dependencies 2

--- main.go
+++ main.go
@@ -3,21 +3,12 @@ package main
 import (
 	"github.com/Grama-Check/Grama-Check-App/handlers"
 
-	"github.com/Grama-Check/Grama-Check-App/middleware"
-
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-
-	// config := cors.DefaultConfig()
-	// config.AllowOrigins = []string{"*"}
-	// config.AllowHeaders = []string{"*"}
-
-	// router.Use(cors.New(config))
-
 	router.Use(static.Serve("/", static.LocalFile("./public", false)))
 
 	authGroup := router.Group("/") //.Use(middleware.AuthMiddleware())
